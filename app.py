7fa25ae61d6963bade67eb80ced856a4505f0bd2
farahfaiez5 <farahfaiez5@gmail.com>
2025-06-24 01:16:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.secret_key = 'your_secret_key'  # Replace with your secret key
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'dr.dcarter24@gmail.com'# Your Gmail address
-app.config['MAIL_PASSWORD'] = ' uskulxbrukmpfmtl'   # Your Gmail password or app-specific password
+app.config['MAIL_USERNAME'] = 'pinaraby9@gmail.com'# Your Gmail address
+app.config['MAIL_PASSWORD'] = ' iflfswrvwyyaiixb'   # Your Gmail password or app-specific password
 
 mail = Mail(app)
 
